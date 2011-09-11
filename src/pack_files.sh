00b7b74ad9b03588a18a06b3422e27d3d1c28d1a
tgk <tgk@cs.au.dk>
2011-09-11 15:02:57 +0200

    Add github page information to scripts

--- src/pack_files.sh
+++ src/pack_files.sh
@@ -1,2 +1,3 @@
 cat jquery-1.6.3.min.js underscore-min.js > all_libs.js
 cat overlay.js isbnfinder.js bookmarklet.js > all_bookmarklet.js
+cat overlay.js isbnfinder.js bookmarklet.github.js > all_bookmarklet_github.js
